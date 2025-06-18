7339dbe2e2cb8165c7c8125345ce3104f68512de
novizarhadisaputra <novizarhadisaputra@gmail.com>
2025-06-18 18:00:34 +0700

    Release: ProvinceResource

---
+++ app/Filament/Resources/ProvinceResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use App\Models\Province;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use App\Filament\Resources\ProvinceResource\Pages;
+use App\Filament\Resources\ProvinceResource\RelationManagers\RegenciesRelationManager;
+
+class ProvinceResource extends Resource
+{
+    protected static ?string $model = Province::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-map';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()->schema([
+                    TextInput::make('name')
+                        ->required()
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(function (Get $get, Set $set, ?string $old, ?string $state) {
+                            if (($get('slug') ?? '') !== Str::slug($old)) {
+                                return;
+                            }
+                            $set('slug', Str::slug($state));
+                        })
+                        ->unique(ignoreRecord: true)
+                        ->maxLength(255),
+                    TextInput::make('slug')
+                        ->readOnly()
+                        ->required(),
+                    TextInput::make('latitude')
+                        ->numeric(),
+                    TextInput::make('longitude')
+                        ->numeric(),
+                ])->columns(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('latitude')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('longitude')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('regencies_count')->counts('regencies'),
+                TextColumn::make('districts_count')->counts('districts'),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RegenciesRelationManager::make(),
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProvinces::route('/'),
+            'create' => Pages\CreateProvince::route('/create'),
+            'edit' => Pages\EditProvince::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Province.php
+++ app/Models/Province.php
@@ -6,6 +6,7 @@ use App\Models\Regency;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Province extends Model
 {
@@ -21,5 +22,15 @@ class Province extends Model
     public function regencies(): HasMany
     {
         return $this->hasMany(Regency::class);
+    }
+
+    /**
+     * Get all of the districts for the Province
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function districts(): HasManyThrough
+    {
+        return $this->hasManyThrough(District::class, Regency::class);
     }
 }
