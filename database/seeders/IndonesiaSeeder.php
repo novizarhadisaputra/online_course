ca005fdc7a2056bb24bc61bd117605ffee278ebd
novizarhadisaputra <novizarhadisaputra@gmail.com>
2025-06-19 06:09:54 +0700

    Release: Add seeder Indonesia Seeder and source sql files

--- database/seeders/IndonesiaSeeder.php
+++ database/seeders/IndonesiaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Jobs\FetchIndonesiaJob;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class IndonesiaSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class IndonesiaSeeder extends Seeder
      */
     public function run(): void
     {
-        FetchIndonesiaJob::dispatch();
+        // FetchIndonesiaJob::dispatch();
+        DB::unprepared(file_get_contents(__DIR__ . "/provinces_202506181825.sql"));
+        DB::unprepared(file_get_contents(__DIR__ . "/regencies_202506181826.sql"));
+        DB::unprepared(file_get_contents(__DIR__ . "/districts_202506181826.sql"));
+        DB::unprepared(file_get_contents(__DIR__ . "/villages_202506181827.sql"));
     }
 }
