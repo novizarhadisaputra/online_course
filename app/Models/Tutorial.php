91a65e432ddade6828dd60ba29af8796f5ab4bd3
novizarhadisaputra <novizarhadisaputra@gmail.com>
2025-06-24 16:10:36 +0700

    Release: Fixing Tutorial

--- app/Models/Tutorial.php
+++ app/Models/Tutorial.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Tutorial extends Model
+class Tutorial extends Model implements HasMedia
 {
-    use HasUuids;
+    use HasUuids, InteractsWithMedia;
 
     protected $guarded = [];
 
