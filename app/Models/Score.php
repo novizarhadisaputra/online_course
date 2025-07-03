ec00bcdeed21bd6ca3e29a191c4fd4bbd5b03dd5
novizarhadisaputra <novizarhadisaputra@gmail.com>
2025-07-03 11:05:49 +0700

    Release: Fixing Score

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\ModelTrait;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    use HasUuids, ModelTrait;
+
+    protected $guarded = [];
 }
